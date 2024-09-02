88fc43115dfcecef4a9a1b62e49db734f03634bf
CaueYanagihara <caueyanagihara@gmail.com>
2024-09-02 20:58:23 -0300

    Checkpoint: feito ajustes gerais de background, formatações e estilos. Vou continuar logo mais.

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./Login.module.css";
 import { useNavigate } from "react-router-dom";
 import { Divider } from 'primereact/divider';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { useTranslation } from "react-i18next";
+import { Card } from "primereact/card";
 
 const Login = () => {
     const {t} = useTranslation();
@@ -29,7 +30,7 @@ const Login = () => {
     }
 
     return (
-        <div className="card w-9 mx-auto my-8">
+        <Card className="card w-9 mx-auto my-8">
             <div className="flex flex-column md:flex-row">
                 <div className="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-3 py-5">
                     <div className="flex flex-wrap justify-content-center align-items-center gap-2">
@@ -40,21 +41,21 @@ const Login = () => {
                         <label className="w-6rem">Senha</label>
                         <InputText onChange={handleChange} name="password" id="password" type="password" placeholder="Digite sua senha" className="w-12rem" />
                     </div>
-                    <Button onClick={login} label={t('button.login')} icon="pi pi-user" className="w-10rem mx-auto"></Button>
+                    <Button label={t('button.login')} icon="pi pi-user" className="w-10rem mx-auto" onClick={login}></Button>
                 </div>
                 <div className="w-full md:w-2">
                     <Divider layout="vertical" className="hidden md:flex">
-                        <b>OR</b>
+                        <span className="font-bold">{t('or')}</span>
                     </Divider>
                     <Divider layout="horizontal" className="flex md:hidden" align="center">
-                        <b>OR</b>
+                        <span className="font-bold">{t('or')}</span>
                     </Divider>
                 </div>
                 <div className="w-full md:w-5 flex align-items-center justify-content-center py-5">
-                    <Button label="Sign Up" icon="pi pi-user-plus" severity="success" className="w-10rem"></Button>
+                    <Button label={t('button.sign-up')} icon="pi pi-user-plus" severity="success" className="w-10rem" onClick={() => { navigate("/register"); }}></Button>
                 </div>
             </div>
-        </div>
+        </Card>
     );
 
 }
